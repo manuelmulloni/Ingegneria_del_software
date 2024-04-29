27f0e4e42b7e01bb6c1f56f1c76e7511708d4208
manuelmulloni <95358949+manuelmulloni@users.noreply.github.com>
2024-04-29 10:51:01 +0200

    esempio di database con possibilità di incorporare metodi già esistenti nella classe database in controller utente

--- Database.py
+++ Database.py
@@ -1,3 +1,22 @@
 import pickle
 
+#esempio database
+class Database:
+    def __init__(self):
+        self.db = "database_utente"
 
+    def aggiungi_database(self, object):
+        database = pickle.dump(object,"database_utente")
+
+    def aggiornamento_database(self, object):
+        database = pickle.dump(object,"database_utente")
+
+    def leggi_database(self, object):
+        database = pickle.load("database_utente")
+
+    def leggi_oggetto_database(self, object):
+        database = pickle.dumps(object,"database_utente")
+    def remove_database(self, object):
+        with open("database_utente.pickle", "rb") as object:
+            database = pickle.load(object)
+            del database[object]
--- proprietario/Controller.py
+++ proprietario/Controller.py
@@ -4,20 +4,21 @@ import Database
 #esempio di controller
 class Controller:
     def __init__(self):
-     def vedere_utente(utente):
+    def vedere_utente(utente):
             return print(utente)
 
-     def change_utente(utente):
+    def change_utente(utente):
+        #importare da database metodo che cambia il file
          utente.nome = input("Nome utente: ")
          utente.email = input("Email utente: ")
          utente.password = input("<PASSWORD>: ")
          return print("Utente modificato")
 
-     def remove_utente(utente):
-         Database.remove(utente)
+    def remove_utente(utente):
+         #importare da database metodo che elimina sul file
          return print("Utente eliminato")
 
-     def create_utente(utente):
+    def create_utente(utente):
          utente.name = input("Nome utente: ")
          utente.email = input("Email utente: ")
          utente.password = input("<PASSWORD>: ")
