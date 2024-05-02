58d4bdadcba01b67e4d4eabfc835f6ae8f040633
manuelmulloni <95358949+manuelmulloni@users.noreply.github.com>
2024-05-02 15:14:40 +0200

    Migliorato e finito il database usando pickle e una lista di oggetti

--- Controller/Controller_Prenotazioni.py
+++ Controller/Controller_Prenotazioni.py
@@ -1,32 +1,30 @@
-import pickle
 
+import pickle
 import Model.Model_Prenotazioni
 import Database
 from Model import Model_Prenotazioni
 
-
 class Controller_Prenotazioni():
     def __init__(self):
         self.prenotazioni = []
+    def insert(self, Utente):
+        with open('Prenotazioni', 'wb') as f:
+            self.prenotazioni.append(Utente)  #inserisce nel file la prenotazione
+            pickle.dump(self.prenotazioni, f)
+            f.close()
+
+    def read(self):
+        with open('Prenotazioni', 'rb') as f:   # leggo le prenotazioni dal file
+             pickle.load(f)
+
+    def update(self, Utente, new):
+        with open('Prenotazioni', 'wb') as f:
+            self.prenotazioni.remove(Utente) #cambia nel file la prenotazione
+            self.prenotazioni.append(new)
+            f.write(pickle.dumps(self.prenotazioni))
 
 
-
-    def read_prenotazioni(self):  # apre il file prenotazioni che viene poi scritto su self.prenotazioni che viene poi printato
-        with open('Prenotazioni', 'rb') as f:
-            self.prenotazioni= pickle.load(f)
-            return print(self.prenotazioni)
-
-
-    def search_prenotazioni(self, username):  # cerca nel file prenotazioni un username e se lotrova stampa la prenotazione
-        with open('Prenotazioni', 'rb') as f:
-            self.prenotazioni= pickle.load(f)
-            if Model_Prenotazioni.Model_Prenotazioni.username == username:
-                return print(Model_Prenotazioni.Model_Prenotazioni.__str__())
-
-    def change_prenotazioni(self, username):
-        with open('Prenotazioni', 'rb') as f:
-            self.prenotazioni = pickle.load(f)
-            if Model_Prenotazioni.Model_Prenotazioni.username == username: #ci devo ragionare su
-                return print("bella")
-
-
+    def delete(self, Utente):
+        with open('Prenotazioni', 'wb') as f:
+            self.prenotazioni.remove(Utente)    #cancella dal file la prenotazione
+            f.write(pickle.dumps(self.prenotazioni))
